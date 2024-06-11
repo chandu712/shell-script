7d5426c897495d3eb75b79409f9ece1dc2b6c00d
chandu712 <chandanaramesh712@gmail.com>
2024-06-11 19:13:46 -0400

    first commit,adding git documentation

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Chandana, How are you?"
+echo "Chandana: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh: What are you doing? How is your work?"
+echo "Chandana: Not bad, Planning to upskill. How about you?"
