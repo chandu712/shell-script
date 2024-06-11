539b65d2aee8b53c1f75780d025ac78c30f96370
chandu712 <chandanaramesh712@gmail.com>
2024-06-11 19:28:09 -0400

    sending values to variables at runtime via arguments or parameters

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Declaration of variable
+PERSON1=$1
+PERSON2=$2
+
+# Referring variable
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1: What are you doing? How is your work?"
+echo "$PERSON2: Not bad, Planning to upskill. How about you?"
