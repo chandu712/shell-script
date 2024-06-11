f742c486e832570d4f389f019ea2c1dcc2efd936
chandu712 <chandanaramesh712@gmail.com>
2024-06-11 19:51:35 -0400

    entering username and pssword before exceution

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter username: "
+read USERNAME  #here USERNAME is a variable
+echo "please enter password: "
+read PASSWORD #here PASSWORD is a variable
+
+echo "username is : $USERNAME, password is : $PASSWORD"
+
