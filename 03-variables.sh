5ff00bdb4f6db8a1965648d778e57e031f537c9c
chandu712 <chandanaramesh712@gmail.com>
2024-06-11 19:19:29 -0400

    first commit,adding git documentation

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Declaration of variables
+PERSON1=Ramesh
+PERSON2=Chandana
+
+# Referring variable
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1: What are you doing? How is your work?"
+echo "$PERSON2: Not bad, Planning to upskill. How about you?"
